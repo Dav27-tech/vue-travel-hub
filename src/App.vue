e83d0c3ef0cb149af1fab8069259e30e35ab997a
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-16 17:39:22 +0200

    Module: App - updated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
-<script setup></script>
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import Destinations from "./components/Destinations.vue";
+import Footer from "./components/Footer.vue";
+import destinations from "./data/destination.js";
+</script>
 
-<template></template>
+<template>
+  <Navbar />
+  <main>
+    <Hero />
+    <destinations />
+  </main>
+  <Footer />
+</template>
