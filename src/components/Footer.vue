b7e59bd356b9b5964042139dc0338e07d3cdf840
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-16 17:35:22 +0200

    Component: Footer - created

---
+++ src/components/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <footer class="bg-dark text-white pt-5 pb-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 md-4">
+          <h4>
+            <i class="bi bi-airolane-fill me-2"></i>
+            TravelHub
+          </h4>
+          <p>Disocver unforgettable experiences around the world.</p>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <h5>Quick Links</h5>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#" class="text-white text-decoration-none"> Home </a>
+            </li>
+
+            <li>
+              <a href="#destinations" class="text-white text-decoration-none">
+                Destinations
+              </a>
+            </li>
+
+            <li>
+              <a href="#packages" class="text-white text-decoration-none">
+                Packages
+              </a>
+            </li>
+
+            <li>
+              <a href="#contact" class="text-white text-decoration-none">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-4">
+          <h5>Newsletter</h5>
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              placeholder="Email address"
+              class="form-control"
+            />
+            <button class="btn btn-primary">subscribe</button>
+          </div>
+          <div>
+            <i class="bi bi-facebook me-3"></i>
+            <i class="bi bi-instagram me-3"></i>
+            <i class="bi bi-twitter me-3"></i>
+            <i class="bi bi-youtube me-3"></i>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <p class="text-center mb-0">2026 TravelHub All rights reserved.</p>
+    </div>
+  </footer>
+</template>
