7277552ef15ddcc04564ed8da24f4d460e712a84
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-16 16:59:35 +0200

    Component: Destinations - created

---
+++ src/components/Destinations.vue
@@ -0,0 +1,19 @@
+<script setup>
+import destinations from "../data/destinations.js";
+import DestinationCard from "./DestinationCard.vue";
+</script>
+
+<template>
+  <section class="py-5" id="destinations">
+    <div class="container">
+      <h2 class="text-center fw-bold mb-5">Popular Destinations</h2>
+      <div class="row g-4">
+        <DestinationCard
+          v-for="destination in destinations"
+          :key="destination.id"
+          :destination="destination"
+        />
+      </div>
+    </div>
+  </section>
+</template>
