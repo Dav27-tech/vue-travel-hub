456c902d1bb795a8fc4a7f8d8042ed5bf544a9c1
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-17 11:02:33 +0200

    Fix import path for destinations data file in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import Destinations from "./components/Destinations.vue";
 import Footer from "./components/Footer.vue";
-import destinations from "./data/destination.js";
+import destinations from "./data/destinations.js";
 </script>
 
 <template>
