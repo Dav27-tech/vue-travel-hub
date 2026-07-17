9210e71c74d4391de5a6456144870274c7bb1d94
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-17 11:02:04 +0200

    Refactor DestinationCard component layout for improved structure

--- src/components/DestinationCard.vue
+++ src/components/DestinationCard.vue
@@ -6,16 +6,22 @@ defineProps({
 
 <template>
   <div class="col-md-4 d-flex">
-    <img
-      class="card-img-top"
-      :src="destination.image"
-      :alt="destination.name"
-    />
-    <div class="card-body d-flex flex-column">
-      <h4>{{ destination.name }}</h4>
-      <p>{{ destination.country }}</p>
-      <h5 class="text-primary">USD ${{ destination.price }}</h5>
-      <button class="btn btn-outline-primary mt-auto">View Package</button>
+    <div class="card shadow w-100">
+      <img
+        :src="destination.image"
+        :alt="destination.name"
+        class="card-img-top"
+      />
+      <div class="card-body d-flex flex-column">
+        <h4>
+          {{ destination.name }}
+        </h4>
+        <p>
+          {{ destination.country }}
+        </p>
+        <h5 class="text-primary">${{ destination.price }}</h5>
+        <button class="btn btn-outline-primary mt-auto">View Package</button>
+      </div>
     </div>
   </div>
 </template>
