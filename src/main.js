c83c7523e66eada704df00f8d4bff8d501703531
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-07-17 11:00:29 +0200

    Add custom styles to main application

--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+import "./styles/style.css";
+
 createApp(App).mount("#app");
